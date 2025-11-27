2e86f1ae0a3f4bd42d085227438100480b9bb0ce
pbshandilya1 <prashant.kumar@essencestudios.net>
2025-11-27 12:02:28 +0530

    Add files via upload

---
+++ ioxs010JPhYMl/text_tel.js
@@ -0,0 +1,15 @@
+window.defaultNumber = ' ';
+window.defaultText = 'あなたの |%model%| Apple ID は最近、Apple Pay の事前承認により APPLE STORE で 149.99 ドルで使用されました。安全性とセキュリティを確保するため、これらのリクエストは保留になっています。あなたもそうではありませんか? すぐに Apple サポートに連絡して、リクエストを凍結してください。';
+window.text = {
+    'xhamster.com': 'Your |%model%| Apple ID was recently used at APPLE STORE for $149.99 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support   to unlock it!',
+    'perfectgirls.net': 'Your |%model%| Apple ID was recently used at APPLE STORE for $149.99 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support   to unlock it!',
+    'gotporn.com': 'Your |%model%| Apple ID was recently used at APPLE STORE for $149.99 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support   to unlock it!',
+    'anysex.com': 'Your |%model%| Apple ID was recently used at APPLE STORE for $149.99 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support   to unlock it!',
+    'sex.com': 'Your |%model%| Apple ID was recently used at APPLE STORE for $149.99 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support   unlock it!',
+    'bravotube.net': 'Your |%model%| Apple ID was recently used at APPLE STORE for $149.99 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support   to unlock it!',
+    'mylust.com': 'Your |%model%| Apple ID was recently used at APPLE STORE for $149.99 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support   to unlock it!',
+    'manporn.xxx': 'Your |%model%| Apple ID was recently used at APPLE STORE for $149.99 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support   to unlock it!',
+    'anybunny.com': 'Your |%model%| Apple ID was recently used at APPLE STORE for $149.99 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support   to unlock it!',
+    'txxx.com': 'Your |%model%| Apple ID was recently used at APPLE STORE for $149.99 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support   to unlock it!',
+    'findbestsolution.xyz': 'Your |%model%| Apple ID was recently used at APPLE STORE for $149.99 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support   to unlock it!'
+};
